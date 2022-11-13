9bc920f76c56f82803bc9c6288e649b5c1299e45
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-13 15:54:57 -0500

    connected server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
@@ -14,4 +28,8 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
